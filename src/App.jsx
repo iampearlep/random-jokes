8a1a4bb140c1909824d83219e1ffd0318a7732af
iampearlep <damilolaprincess4@gmail.com>
2023-02-17 13:56:28 +0100

    update app.js and api.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,61 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 //import axios from "axios";
 import { jokeOptions } from "./api";
 
-const apiUrl = "https://humor-jokes-and-memes.p.rapidapi.com/jokes/";
+const apiUrl = "https://jokeapi-v2.p.rapidapi.com/joke/";
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState("");
   
-  const getRandomJokeData = () => {
-   fetch(`${apiUrl}random?max-length=200&include-tags=one_liner&min-rating=7&exclude-tags=nsfw&keywords=rocket`, jokeOptions)
-   .then(res => res.json())
-   .then((json) => setData(json));
-  };
+  
 
-  useEffect(() =>{
-    getRandomJokeData();
+  const getRandomJoke = () => {
+    const jokeDataFetch = fetch(`${apiUrl}Any?format=json&idRange=0-150&blacklistFlags=nsfw%2Cracist`,jokeOptions);
+    Promise.all([jokeDataFetch])
+  .then(async(response) => {
+    const jokeResponse = await response[0].json();
+    setData(jokeResponse);
+    console.log(jokeResponse)
+  })
+  };
+  
+  useEffect(() => {
+    getRandomJoke();
   }, []);
 
+  const handleRefresh = () => {
+    window.location.reload();
+  };
 
-  
-  
- 
-  return <div>
-    {data.joke}
-  </div>;
+  return (
+    <div>
+      <p> {data.setup}</p>
+      <p> {data.delivery}</p>
+      <button onClick={handleRefresh}>Generate Joke</button>
+    </div>
+  );
 };
 
 export default App;
 
-
 // React.useEffect(() => {
-  //   axios.get(apiUrl).then((res) => {
-  //     setData(res.data);
-  //     console.log(data);
-  //   });
-  // }, []);
+//   axios.get(apiUrl).then((res) => {
+//     setData(res.data);
+//     console.log(data);
+//   });
+// }, []);
 
-  // axios
-  //   .get("https://jokeapi-v2.p.rapidapi.com/joke/Any")
-  //   .then((res) => {
-  //     setData(res.data);
-  //     console.log(res.data);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
- //   fetch(`${apiUrl}Any?format=json&contains=C%2523&idRange=0-150&blacklistFlags=nsfw%2Cracist`, options)
-	// .then(response => response.json())
-	// .then(response => console.log(response))
-	// .catch(err => console.error(err));
-  // }
+// axios
+//   .get("https://jokeapi-v2.p.rapidapi.com/joke/Any")
+//   .then((res) => {
+//     setData(res.data);
+//     console.log(res.data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+//   fetch(`${apiUrl}Any?format=json&contains=C%2523&idRange=0-150&blacklistFlags=nsfw%2Cracist`, options)
+// .then(response => response.json())
+// .then(response => console.log(response))
+// .catch(err => console.error(err));
+// }
